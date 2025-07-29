6c4c1bf3ae456beedb7b70ed0cb4b0ea17f95ba0
agnostic79 <marti_x@abv.bg>
2025-07-30 00:34:06 +0300

    Authentification

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,12 +2,25 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Resumind" }, { name: "description", content: "Smart feedback for your dream job!" }];
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
