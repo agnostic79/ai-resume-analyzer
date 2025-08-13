e7321c4a15d3c461e3eced2187e0d3d58d03ca79
agnostic79 <marti_x@abv.bg>
2025-08-13 17:45:50 +0300

    Cound storage, PDFtoImage conversion, Key-Value storage, AI feedback

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
   index("routes/home.tsx"),
   route("/.well-known/appspecific/com.chrome.devtools.json", "routes/debug-null.tsx"),
   route("/auth", "routes/auth.tsx"),
+  route("/upload", "routes/upload.tsx"),
 ] satisfies RouteConfig;
